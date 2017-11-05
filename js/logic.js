d35b7eb638c89c2a8f1253af15c9cd4183e4004c
Alex_Tymchenko <adellantado@gmail.com>
2017-11-05 12:54:44 +0200

    add navigation;remove integration from scripts

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,17 @@
 $("#scroll_button").click(function() {
   $('html,body').animate({
-        scrollTop: $("#map_container").offset().top},
+        scrollTop: $("#map_nav").offset().top},
       2000);
+});
+
+$("#about-btn").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#about-nav").offset().top},
+        2000);
+});
+
+$("#footer-btn").click(function() {
+    $('html,body').animate({
+            scrollTop: $("#footer-nav").offset().top},
+        2000);
 });
